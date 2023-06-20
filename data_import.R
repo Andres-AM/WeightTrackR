fcce8fca377594f1251552ba65b45f820d49d7dd
Andres-AM <andes.acunamarroquin@gmail.com>
2023-06-20 11:46:00 +0200

    adding confidence intervals

--- data_import.R
+++ data_import.R
@@ -1,6 +1,6 @@
 
 ## Range of data visualization in days
-t_interval <- as.double(upr_lim-lwr_lim)
+t_interval <- as.double(lim_upr-lim_lwr)
 
 ## Data is available in the data folder under different text files
 dr_1 <- read_delim("Data/body_mass.txt", delim = ",", show_col_types = F)
@@ -11,7 +11,7 @@ dr_7 <- read_delim("Data/prot.txt", delim = ",", show_col_types = F)
 
 ## Data import, merging and setting the start date to zero
 dr <- reduce(list(dr_1,dr_2,dr_3),full_join, by = "Date") %>% 
-  mutate( Date = date(Date), n_day = as.double(Date) - as.double(lwr_lim)) %>% 
+  mutate( Date = date(Date), n_day = as.double(Date) - as.double(lim_lwr)) %>% 
   reduce(.x = list(.,dr_4,dr_7), .f = full_join, by = "Date") %>%
   
   ## Renaming variables for better clarity
@@ -26,7 +26,7 @@ dr <- reduce(list(dr_1,dr_2,dr_3),full_join, by = "Date") %>%
           n_week =  n_day %/%7  ) %>% 
   
   ## Filtering missing values    
-  filter(!(is.na(body_mass)) & Date > lwr_lim) %>% 
+  filter(!(is.na(body_mass)) & Date > lim_lwr) %>% 
   select( Date,n_day,n_week,everything()) %>% 
 
 ## Summarizing by date 
@@ -39,54 +39,23 @@ dr <- reduce(list(dr_1,dr_2,dr_3),full_join, by = "Date") %>%
                    mean, na.rm = TRUE))
 
 ## Predictive model for Body Mass and Fat Percentage
-# model_bodymass <- lm( body_mass ~  poly(x = n_week,degree = ord,raw = T), dr[dr$n_week > last(dr$n_week) - w_mod,])
-# model_fatperc  <- lm( fat_perc  ~  poly(x = n_week,degree = ord,raw = T), dr[dr$n_week > last(dr$n_week) - w_mod,])
-
 model_bodymass <- lm( body_mass ~    n_week, dr[dr$n_week > last(dr$n_week) - w_mod,])
 model_fatperc  <- lm( fat_perc  ~    n_week, dr[dr$n_week > last(dr$n_week) - w_mod,])
 
+## Adding confidence intervals
+model_data <- dr %>%
+  data_grid( n_week = seq(last(n_week)-w_mod,t_interval %/%7 )) %>%
+  data.frame(predict(model_bodymass,.,interval = "confidence"), 
+             predict(model_fatperc, .,interval = "confidence")) %>% 
+  as_tibble() 
 
-model_data <- dr %>% 
-  data_grid( n_week = seq(last(n_week)-w_mod,t_interval %/%7 )) %>%  
-  add_predictions( model = model_bodymass , var = "bodymass_pred",) %>% 
-  add_predictions( model = model_fatperc , var = "fatperc_pred") 
-# 
-# ## new version 
-# model_data <- dr %>% 
-#   data_grid( n_week = seq(last(n_week)-w_mod,t_interval %/%7 )) %>% 
-#   augment(model_bodymass, newdata = .)
-# %>%
-#   mutate(.fitted.low = .fitted - qnorm((1 - 0.9) / 2) * .se.fit,
-#          .fitted.low = .fitted + qnorm((1 - 0.9) / 2) * .se.fit)
-# 
-# pred1 <- predict(object = model_bodymass, newdata = model_data, interval = "prediction") %>%  as_tibble()
-# names(pred1 = )
-# 
-# 
-# m <- lm(mpg ~ disp + cyl)
-# d %>%
-#   data_grid(cyl, .model = m) %>%
-#   augment(m, newdata = .) %>%
-#   mutate(.fitted.low = .fitted - qnorm((1 - 0.9) / 2) * .se.fit,
-#          .fitted.low = .fitted + qnorm((1 - 0.9) / 2) * .se.fit)
-# %>% 
-#   mutate()
-# 
-# 
-# a <- predict(object = model_bodymass,model_data, interval = "prediction")
-# # %>% 
-# predict(object = model_bodymass,model_data$n_week)
-#   
-#   mutate(  preds=predict(model_bodymass,n_week))
-# 
-# 
-# data_2 <- map(model_dna$model_dna, function(x) {
-#   preds=predict(x, data_1, se.fit=TRUE)
-#   mutate(data_1, fit=preds$fit, lwr=fit-preds$se.fit*1.96, upr=fit+preds$se.fit*1.96)
-# })
+names(model_data) <- c("n_week",
+                       "bodymass_pred","bodymass_pred_lwr","bodymass_pred_upr",
+                       "fatperc_pred","fatperc_pred_lwr","fatperc_pred_upr"
+                       )
 
 ## Adding the prediction to the main data set and renaming the week variable to date
 df_clean <- dr %>% 
   full_join(model_data,by =  "n_week") %>% 
   rename(date = n_week) %>%
-  select(date,body_mass,bodymass_pred,fat_perc, fatperc_pred,kcal,lean_perc, prot_g, fat_mass, lean_mass)
+  select(date,body_mass,bodymass_pred,bodymass_pred_lwr,bodymass_pred_upr,fat_perc,fatperc_pred,fatperc_pred_lwr,fatperc_pred_upr,kcal,lean_perc, prot_g, fat_mass, lean_mass)
