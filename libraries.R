087068498c0339a374109e1d49de54c3ff6eb7ae
Andres-AM <andres.acunamarroquin@gmail.com>
2024-11-29 09:38:12 +0100

    data update

--- libraries.R
+++ libraries.R
@@ -5,12 +5,7 @@ library(modelr)
 library(plotly)
 library(DT)
 library(shinydashboard)
-# library(leaflet)
-# library(tmap)
-# library(sf)
-# library(terra)
-# library(spData)
-# library(spDataLarge)
+
 
 ## Other settings 
 theme_set(theme_bw())
